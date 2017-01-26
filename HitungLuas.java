4d6b49ec59ec29cca3890832fc714b4710918a6c
Muhammad Azizul Jabbar <abu.abyan.alif@gmail.com>
2017-01-26 21:27:43 +0700

    Penambahan luas lingkaran dan edit luas persegi panjang

--- HitungLuas.java
+++ HitungLuas.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
-public class luassegipanjang {
+public class HitungLuas {
 public static void main(String[] args) {
+//Luas Persegi Panjang
    int panjang;
    int lebar;
-   double luas;
+   double luaspersegipanjang;
 
    Scanner scan = new Scanner(System.in);
    System.out.println("Menghitung Luas Persegi Panjang");
@@ -12,7 +13,17 @@ public static void main(String[] args) {
    System.out.print("Masukan Lebar: ");
    lebar = scan.nextInt();
   
-   luas = (lebar * panjang);
-   System.out.println("Luas Segi Panjang = " +luas);
+   luaspersegipanjang = (lebar * panjang);
+   System.out.println("Luas Segi Panjang = " +luaspersegipanjang);
+//Luas Lingkaran
+Scanner input =new Scanner(System.in);
+    double phi=3.14;
+    double jari,luaslingkaran;
+    System.out.print("Berapa Jari-jari lingkaran:");
+    jari=input.nextDouble();
+    
+    luaslingkaran = phi*jari*jari;
+    
+    System.out.println("Luas Lingkaran = " + (double)luaslingkaran);
     }
 }
