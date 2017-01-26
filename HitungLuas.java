d4964a583cc215779f04838a31e92dbab8701e41
Muhammad Azizul Jabbar <abu.abyan.alif@gmail.com>
2017-01-26 22:09:32 +0700

    Penambahan luas segitiga dan edit luas persegi panjang

--- HitungLuas.java
+++ HitungLuas.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
-public class luassegipanjang {
+public class HitungLuas {
 public static void main(String[] args) {
+//Luas Persegi Panjang
    int panjang;
    int lebar;
-   double luas;
+   double luaspersegipanjang;
 
    Scanner scan = new Scanner(System.in);
    System.out.println("Menghitung Luas Persegi Panjang");
@@ -12,7 +13,15 @@ public static void main(String[] args) {
    System.out.print("Masukan Lebar: ");
    lebar = scan.nextInt();
   
-   luas = (lebar * panjang);
-   System.out.println("Luas Segi Panjang = " +luas);
-    }
+   luaspersegipanjang = (lebar * panjang);
+   System.out.println("Luas Segi Panjang = " +luaspersegipanjang);
+//Luas Segitiga
+Scanner input =new Scanner(System.in);
+System.out.print("Masukkan alas: ");
+int alas = input.nextInt();
+System.out.print("Masukkan tinggi: ");
+int tinggi = input.nextInt();
+double luas = (double)(alas*tinggi)/2;
+System.out.print("Luas Segitiga adalah :" +luas);
+}
 }
